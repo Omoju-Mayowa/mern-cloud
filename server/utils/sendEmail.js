60207b468c7143d7f03e074d1b51d9b192684bd9
NOX <133047272+Omoju-Mayowa@users.noreply.github.com>
2026-01-18 12:29:50 +0100

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,11 +1,19 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 const sendEmail = async function (to, subject, message) {
