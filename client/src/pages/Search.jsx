5401e2f848a7483ba19737d6c8a4af77ce1e6b7f
NOX <133047272+Omoju-Mayowa@users.noreply.github.com>
2026-01-18 12:32:14 +0100

    Update Search.jsx

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -9,6 +9,7 @@ import ReactTimeAgo from 'react-time-ago'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
 import ru from 'javascript-time-ago/locale/ru.json'
+import Loader from './components/Loader'
 
 TimeAgo.addDefaultLocale(en)
 TimeAgo.addLocale(ru)
@@ -189,7 +190,7 @@ const Search = () => {
 
                 {loading && (
                     <div className="center">
-                        <p>Searching...</p>
+                      <Loader size='small' />
                     </div>
                 )}
 
