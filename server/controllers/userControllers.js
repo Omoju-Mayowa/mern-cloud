97c7d5980e008ac6d6ec5290723d0b451d4256e6
n0x <133047272+Omoju-Mayowa@users.noreply.github.com>
2026-01-19 21:58:44 +0100

    Update userControllers.js

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -150,7 +150,7 @@ export const loginUser = async (req, res, next) => {
         const token = jwt.sign(
             { id: user._id, name: user.name }, 
             process.env.JWT_SECRET, 
-            { expiresIn: '6h' }
+            { expiresIn: '1m' }
         );
 
         res.status(200).json({ 
