e994d55522fcad72e26ec246722e3f621cbd2c8b
n0x <133047272+Omoju-Mayowa@users.noreply.github.com>
2026-01-19 22:34:35 +0100

    Update DeletePost.jsx

--- client/src/pages/DeletePost.jsx
+++ client/src/pages/DeletePost.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 // Import YOUR custom axios
-import axios from '../axios' 
+import axios from './components/axios' 
 import { UserContext } from './components/context/userContext'
 
 const DeletePost = ({ postId }) => {
